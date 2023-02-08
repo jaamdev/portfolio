e7cae95c6ef8ccacf72c652ef6241995b43d4576
jaamdev <95362000+jaamdev@users.noreply.github.com>
2023-02-08 05:10:19 +0100

    Updated few files
    Actualización del JSON y mejorar el diseño de los botones de prácticas.

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const ulProyects = document.querySelector('.projects__ctn')
-const ulPractices = document.querySelector('.practices__ctn')
+const ulProyects = document.querySelector(".projects__ctn");
+const ulPractices = document.querySelector(".practices__ctn");
 
-fetch('./db.json')
-	.then(res => res.json())
-	.then(items => {
-		items.forEach(item => {
-			if (item.projectName) {
-				const li = document.createElement('li')
-				li.setAttribute('class', 'li')
-				li.innerHTML = `
+fetch("./db.json")
+  .then((res) => res.json())
+  .then((items) => {
+    items.forEach((item) => {
+      if (item.projectName) {
+        const li = document.createElement("li");
+        li.setAttribute("class", "li");
+        li.innerHTML = `
 					<a class="a" href="${item.url}" target="_blank" rel="noopener noreferrer">
 						<img class="img" src="${item.urlImg}" alt="Imagen del proyecto ${item.projectName}" />
 						<h3 class="title">${item.projectName}</h3>
-						<p class="description">${item.projectDescription}</p>
+						<span class="description">${item.projectDescription}</span>
 					</a>
-				`
-				ulProyects.appendChild(li)
-			}
-			else if(item.name){
-				const li = document.createElement('li')
-				li.setAttribute('class', 'li')
-				li.innerHTML = `
+				`;
+        ulProyects.appendChild(li);
+      } else if (item.name) {
+        const li = document.createElement("li");
+        li.setAttribute("class", "li");
+        li.innerHTML = `
 					<a class="a" href="${item.url}" target="_blank" rel="noopener noreferrer">
-						<p class="title">${item.name}</p>
+						<span class="title">${item.name}</span>
 					</a>
-				`
-				ulPractices.appendChild(li)
-			}
-		})
-	})
-	.catch(() => {
-		const li = document.createElement('li')
-		li.setAttribute('class', 'li errorText')
-		li.textContent = "¡Ups! ¡Error al cargar los elementos!"
-		ulPractices.appendChild(li)
-	})
+				`;
+        ulPractices.appendChild(li);
+      }
+    });
+  })
+  .catch(() => {
+    const li = document.createElement("li");
+    li.setAttribute("class", "li errorText");
+    li.textContent = "¡Ups! ¡Error al cargar los elementos!";
+    ulPractices.appendChild(li);
+  });
